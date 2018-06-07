a36f8d04765a1012905b151ad92b5a17d45bc1d2
Erick David Mendoza Velasco <erick_tec@outlook.com>
2018-06-06 20:25:28 -0500

    Validation

--- Erick/p1.py
+++ Erick/p1.py
@@ -1,12 +1,26 @@
 def piramidefor():
-	for x in range (5,0,-1):
+	height = validacionEnteros("Escribe la altura de la piramide")
+	while height<=0:
+		height = validacionEnteros("Escribe la altura de la piramide mayor a cero")
+	for x in range (height,0,-1):
 		print("*"*x)
 
 def piramidewhile():
-	x=5
-	while x >= 1:
-		print("*"*x)
-		x = x-1
+	height = validacionEnteros("Escribe la altura de la piramide")
+	while height<=0:
+		height = validacionEnteros("Escribe la altura de la piramide mayor a cero")
+	while height >= 1:
+		print("*"*height)
+		height = height-1
+
+def validacionEnteros(mensaje):
+    while True:
+        try:
+            num=int(input(mensaje))
+            break
+        except ValueError:
+            print("Solo se puede introducir números enteros")
+    return num
 
 piramidefor()
 piramidewhile()
