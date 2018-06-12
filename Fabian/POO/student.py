bdb323eb3f3098caa960e41bcf3536506f24def3
Fabian Ramirez <A01630024@itesm.mx>
2018-06-11 21:59:04 -0500

    student

--- Fabian/POO/student.py
+++ Fabian/POO/student.py
@@ -1,5 +1,5 @@
 class Student(object):
-    def _init_(self,name, score):
+    def __init__(self,name, score):
         self.name=name
         self.score=score
         print(f'hello {name}')
@@ -8,4 +8,7 @@ class Student(object):
         print(f'name: {self.name}')
         print(f'score: {self.score}')
 
-student=Student()
+student1=Student('Fabian', [99,99,100])
+student2=Student('AnotherGuy', [12,43,79])
+
+student2.print_details()
