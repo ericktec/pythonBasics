be62e14441d0271538b394fce3cd3fa8b1a8d382
Fabian Ramirez <A01630024@itesm.mx>
2018-06-13 20:50:42 -0500

    dictionaries.py edited

---
+++ Fabian/POO/bank/main.py
@@ -0,0 +1,37 @@
+'''
+    Bank Account
+
+'''
+class BankAccount:
+    def __init__(self):
+        self.balance=0
+    def deposit(self, amount):
+        self.balance+=amount
+        return self.balance
+    def withdraw(self, amount):
+        self.balance-=amount
+        return self.balance
+
+def main():
+    bankAccount1=BankAccount()
+    while True:
+        print('------------------------------------------')
+        print('Inserta la opcion deseada:')
+        print('a) Depositar')
+        print('b) Retirar')
+        print('cualquier tecla) SAlIR')
+        option=input('...')
+        if (option=='a'):
+                amount=int(input('inserta la cantidad a depositar... '))
+                print(f'Tu saldo es de ${bankAccount1.deposit(amount)}')
+        elif(option=='b'):
+            amount=int(input('inserta la cantidad a retirar... '))
+            if(amount>bankAccount1.balance):
+                print('No tienes el saldo suficiente :v')
+                print(f'Tu saldo es de ${bankAccount1.balance}')
+            else:
+                print(f'Tu saldo es de ${bankAccount1.withdraw(amount)}')
+        else:
+            break
+
+main()
